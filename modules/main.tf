bce1d467e0cdd80d0e76fa039de272e16ddc8b7e
babumg1210 <babumg1210@outlook.com>
2025-02-18 19:22:47 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,3 +1,23 @@
+## Step 1 : Create an S3 Bucket
+resource "random_id" "random_hex" {
+  byte_length = 8
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = format("%s-%s", var.bucket_name, random_id.random_hex.hex)
+  acl    = var.acl
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+
+
+
+
+
+
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
   acl    = var.acl
